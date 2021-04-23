b166f1991d8f1764c8b3ada350201a7d5c7c7d09
dave1b <57140925+dave1b@users.noreply.github.com>
2021-04-23 14:04:54 +0200

    Added new Test-cases.

--- src/test/java/ch/hslu/a1/SortTest.java
+++ src/test/java/ch/hslu/a1/SortTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class SortTest {
@@ -12,25 +13,57 @@ class SortTest {
 	private static final Logger LOG = LogManager.getLogger(SortTest.class);
 	static int[] intArray = { 0, 4, 6, 565, 324, 6543, 27, 34 };
 	static int[] intArraySorted = { 0, 4, 6, 27, 34, 324, 565, 6543 };
+	static char[] charArray = { 0, 4, 6, 565, 324, 6543, 27, 34 };
+	static char[] charArraySorted = { 0, 4, 6, 27, 34, 324, 565, 6543 };
 	Sort sort = new Sort();
-
 	
+
+	// Quick Sort
+	@Test
+	void testQuickSort1() {
+		LOG.debug("Array mit: 8 Items");
+		assertArrayEquals(charArraySorted, Sort.quickSort(charArray, 0, (intArray.length - 1)));
+	}
+
+	@Test
+	void testQuickSort2() {
+		int length = 100;
+//		Sort.printCharArray(Sort.quickSort(Sort.randomChars(length), 0, length - 1));
+	}
+	
+	
+	@Test
+	void testQuickSort3() {
+		int length = 125_000;
+		Sort.quickSort(Sort.randomChars(length), 0, length - 1);
+		// Time neede for 100: 0.00s for 125k: 0.112s for 250k: 0.31 500k: 
+	}
+	@Test
+	void testQuickSort4() {
+		int length = 250_000;
+		Sort.quickSort(Sort.randomChars(length), 0, length - 1);
+		// Time neede for 100: 0.00s for 125k: 0.112s for 250k: 0.31 500k: 
+	}
+	
+	
+
 	// Shell Sort
+	@Disabled
 	@Test
 	void testShellSort1() {
 		LOG.debug("Array mit: 8 Items");
 		assertArrayEquals(intArraySorted, Sort.shellSort(intArray));
 	}
-	
-	
-	
-	
+
 	// Direktes Austausche -- Bubble Sort
+	@Disabled
 	@Test
 	void testBubbleSort1() {
 		LOG.debug("Array mit: 8 Items");
 		assertArrayEquals(intArraySorted, Sort.bubbleSort(intArray));
 	}
+
+	@Disabled
 	@Test
 	void testBubbleSort2() {
 		int x = 50000;
@@ -41,6 +74,8 @@ class SortTest {
 		LOG.debug("Array mit: " + x + " Items");
 		Sort.bubbleSort(intArrayBig);
 	}
+
+	@Disabled
 	@Test
 	void testBubbleSort3() {
 		int x = 100000;
@@ -51,6 +86,8 @@ class SortTest {
 		LOG.debug("Array mit: " + x + " Items");
 		Sort.bubbleSort(intArrayBig);
 	}
+
+	@Disabled
 	@Test
 	void testBubbleSort4() {
 		int x = 200000;
@@ -61,22 +98,16 @@ class SortTest {
 		LOG.debug("Array mit: " + x + " Items");
 		Sort.bubbleSort(intArrayBig);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	// Direktes Auswählen -- Selection Sort
+	@Disabled
 	@Test
 	void testSelectionSort1() {
 		LOG.debug("Array mit: 8 Items");
 		assertArrayEquals(intArraySorted, Sort.selectionSort(intArray));
 	}
 
+	@Disabled
 	@Test
 	void testSelectionSort2() {
 		int x = 50000;
@@ -87,6 +118,8 @@ class SortTest {
 		LOG.debug("Array mit: " + x + " Items");
 		Sort.selectionSort(intArrayBig);
 	}
+
+	@Disabled
 	@Test
 	void testSelectionSort3() {
 		int x = 100000;
@@ -97,6 +130,8 @@ class SortTest {
 		LOG.debug("Array mit: " + x + " Items");
 		Sort.selectionSort(intArrayBig);
 	}
+
+	@Disabled
 	@Test
 	void testSelectionSort4() {
 		int x = 200000;
@@ -108,15 +143,15 @@ class SortTest {
 		Sort.selectionSort(intArrayBig);
 	}
 
-	
 	// Direktes Einfügen -- Insertion Sort
-
+	@Disabled
 	@Test
 	void testInsertionSort1() {
 		LOG.debug("Array mit: 8 Items");
 		assertArrayEquals(intArraySorted, Sort.insertionSort(intArray));
 	}
 
+	@Disabled
 	@Test
 	void testInsertionSort2() {
 		int x = 50000;
@@ -128,6 +163,7 @@ class SortTest {
 		Sort.insertionSort(intArrayBig);
 	}
 
+	@Disabled
 	@Test
 	void testInsertionSort3() {
 		int x = 100000;
@@ -139,6 +175,7 @@ class SortTest {
 		Sort.insertionSort(intArrayBig);
 	}
 
+	@Disabled
 	@Test
 	void testInsertionSort4() {
 		int x = 200000;
